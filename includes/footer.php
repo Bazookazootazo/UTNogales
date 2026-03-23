fdac5020381b1aa3932661ea93d0948a2a1d2617
Maximiliano Valdez Hernandez <maximilianovaldezhernandez01@gmail.com>
2026-03-23 08:48:15 -0700

    KK FUERA

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<?php include_once 'includes/header.php'; ?>
-
-<main class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold">Panel de Control</h1>
-    <button class="btn btn-primary">Acción Rápida</button>
-</main>
-
-<?php include_once 'includes/footer.php'; ?>
